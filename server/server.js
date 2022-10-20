ca9210bde8c603fdccdce18a0554c19b6f4b69d5
Tiara Temple <tmtemple7@gmail.com>
2022-10-20 13:46:09 -0500

    add rollbar

--- server/server.js
+++ server/server.js
@@ -7,6 +7,17 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '5e5624c1a4a94fc195ac2a9993925bf2',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 
 const { getHTML, getCSS, getJS } = require('./controller')
 
