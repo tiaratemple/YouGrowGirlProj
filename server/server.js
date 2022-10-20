3793f4ca637bdfe622a355381d4eb3994e98e9af
Tiara Temple <tmtemple7@gmail.com>
2022-10-19 22:25:41 -0500

    Port fix for heroku

--- server/server.js
+++ server/server.js
@@ -14,4 +14,6 @@ app.get('/' , getHTML)
 app.get('/css' , getCSS)
 app.get('/js', getJS )
 
-app.listen(4000, console.log('Server running on 4000'))
+const port = process.env.PORT || 4000
+
+app.listen(port, console.log(`Server running on ${port}`))
