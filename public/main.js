e77fa03426d04feca4ead474f9f48e43cdf61cea
Tiara Temple <tmtemple7@gmail.com>
2022-10-25 14:38:43 -0500

    first commit with changes for capstone proj

--- public/main.js
+++ public/main.js
@@ -2,10 +2,17 @@ let plant = document.querySelector('img')
 
 const helloAlert = evt => {
     alert("Well, ALOE there!")
-}
+};
 
 const plantBtn = document.getElementById("plantButton")
 
+const getPlantFact = () => {
+    axios.get("http://localhost:4000/api/plantFact/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+        });
+};
 
 
 
--- server/controller.js
+++ server/controller.js
@@ -5,17 +5,17 @@ path = require('path')
 module.exports = {
     getHTML : (req, res) => {
         console.log(__dirname)
-        res.sendFile(path.join(__dirname, '../public/index.html'))
+        res.sendFile(path.join(__dirname, './public/index.html'))
     },
     getCSS: (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/styles.css'))
+        res.sendFile(path.join(__dirname, './public/styles.css'))
 
     }, 
     getJS: (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/main.js'))
+        res.sendFile(path.join(__dirname, './public/main.js'))
     },
     getPlantFact: (req, res) => {
-        const plantFacts = ["70,000 plant species are utilized for medicine.", "Bamboo can grow 35 inches in a DAY!", "Plants with too much dust on their leaves won't get the sunshine they need.", "It is better to underwater your plants than to overwater them.", "In Holland, during the 1600's, tulips were more valuable than gold."];
+        const plantFacts = ["70,000 plant species are utilized for medicine.", "Bamboo can grow 35 inches in a DAY!", "Plants with too much dust on their leaves won't get the sunshine they need.", "It is better to underwater your plants than to overwater them.", "In Holland, during the 1600's, tulips were more valuable than gold.", "Ingesting oleander leaves can cause death.", "'Iris' means 'rainbow' in Greek.", "Trees are the longest-living organisms on earth.", "The calcium in eggshells can help neutralize the pH in your soil.", "The antifungal properties of cinnamon can help prevent seedlings from dying due to fungal diseases."];
 
         let randomIndex = Math.floor(Math.random() * plantFacts.length);
         let randomPlantFact = plantFacts[randomIndex];
