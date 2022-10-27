a16346e16bc7242f048b0626553888f575bcb33f
Tiara Temple <tmtemple7@gmail.com>
2022-10-26 23:35:53 -0500

    both endpoints working & UI is showing data correctly

--- public/main.js
+++ public/main.js
@@ -1,23 +1,67 @@
-let plant = document.querySelector('img')
-
-const helloAlert = evt => {
-    alert("Well, ALOE there!")
-};
-
+const plantsContainer = document.getElementById('plants-container')
+const form = document.getElementById('form');
 const plantBtn = document.getElementById("plantButton")
 
+const baseURL = `http://localhost:4000/api/plants`
+
+const plantsCallback = ({ data: plants }) => createPlantCard(plants)
+const errCallback = err => console.log(err.response.data)
+let inputField;
+
+const getPlants = () => {
+    axios.get(baseURL)
+    .then(plantsCallback)
+    .catch(errCallback)
+}
+
+const updateLikes = (id) => {
+    axios.put(`${baseURL}/${id}`)
+    then(plantsCallback)
+    .catch(errCallback)
+}
+
 const getPlantFact = () => {
-    axios.get("http://localhost:4000/api/plantFact/")
+    axios.get("http://localhost:4000/api/plantFacts/")
         .then(res => {
             const data = res.data;
             alert(data);
         });
+}
+
+function submitHandler(e) {
+    e.preventDefault();
+    inputField = document.getElementById('input')
+    getPlants();
+}
+
+function createPlantCard(plants) {
+    const plant = plants.find((plant) => {
+        if (inputField.value === plant.name) {
+            return plant
+        }
+    });
+
+    const plantCard = document.createElement('div');
+
+    plantCard.classList.add('plant-card');
+
+    plantCard.innerHTML = `
+        <img alt='plant' src=${plant.img} class="movie-cover"/>
+        <p class="plant-title">${plant.name}</p>
+        <div class="care-tips-container">
+            <ul>
+                <li>${plant.careTips[0].water}</li>
+                <li>${plant.careTips[1].sunlight}</li>
+                <li>${plant.careTips[2].difficulty}</li>
+            </ul>
+        </div>
+            ${plant.Like}
+        <button onClick="updateLikes(${plant.id})">
+            Upvote
+        </button>
+    `
+    plantsContainer.appendChild(plantCard)
 };
 
-
-
-plantBtn.addEventListener('click', getPlantFact)
-
-
-
-plant.addEventListener('click', helloAlert)
+plantBtn.addEventListener('click', getPlantFact);
+form.addEventListener('submit', submitHandler);
--- server/server.js
+++ server/server.js
@@ -5,29 +5,33 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 // include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '5e5624c1a4a94fc195ac2a9993925bf2',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+// var Rollbar = require('rollbar')
+// var rollbar = new Rollbar({
+//   accessToken: '5e5624c1a4a94fc195ac2a9993925bf2',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// })
 
 // record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+//rollbar.log('Hello world!')
 
 
 
-const { getHTML, getCSS, getJS, getPlantFact, getRollbar} = require('./controller')
+const { getPlants, likePlants, getPlantFact} = require('./controller')
 
-app.get('/' , getHTML)
-app.get('/css' , getCSS)
-app.get('/js', getJS )
-app.get('/' , getPlantFact)
-app.get('rollbar', getRollbar)
 
-const port = process.env.PORT || 4000
+//app.get('/' , getHTML)
+//app.get('/css' , getCSS)
+//app.get('/js', getJS )
+app.get('/api/plants', getPlants)
+app.put('/api/plants/:id', likePlants)
+app.get('/api/plantFacts', getPlantFact)
+//app.get('rollbar', getRollbar)
 
-app.listen(port, console.log(`Server running on ${port}`))
+//const port = process.env.PORT || 4000
+
+//app.listen(port, console.log(`Server running on ${port}`))
+app.listen(4000, () => console.log(`running on 4000`))
