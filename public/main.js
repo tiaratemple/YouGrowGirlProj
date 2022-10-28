45c3d071ec092dc3ecfa17b3a6f1739e2ae78201
Tiara Temple <tmtemple7@gmail.com>
2022-10-27 23:01:36 -0500

    icons, cards for plants,flexbox

--- public/main.js
+++ public/main.js
@@ -1,73 +1,105 @@
-const plantsContainer = document.getElementById('plants-container');
-const form = document.getElementById('form');
+const plantsContainer = document.getElementById("plants-container");
+const form = document.getElementById("form");
 const plantBtn = document.getElementById("plantButton");
 
 const baseURL = `http://localhost:4000/api/plants`;
 
 const plantsCallback = ({ data: plants }) => createPlantCard(plants);
 const updateLikesCallback = ({ data: plant }) => updateLikeValue(plant);
-const errCallback = err => console.log(err.response.data);
+const errCallback = (err) => console.log(err.response.data);
 let inputField;
 
 const getPlants = () => {
-    axios.get(baseURL)
-    .then(plantsCallback)
-    .catch(errCallback)
+  axios.get(baseURL).then(plantsCallback).catch(errCallback);
 };
 
 const updateLikes = (id) => {
-    axios.put(`${baseURL}/${id}`)
-    .then(updateLikesCallback)
-    .catch(errCallback)
+  axios.put(`${baseURL}/${id}`).then(updateLikesCallback).catch(errCallback);
 };
 
 const getPlantFact = () => {
-    axios.get("http://localhost:4000/api/plantFacts/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-        });
+  axios.get("http://localhost:4000/api/plantFacts/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
 };
 
-const submitHandler =(e) => {
-    e.preventDefault();
-    inputField = document.getElementById('input')
-    getPlants();
+const submitHandler = (e) => {
+  e.preventDefault();
+  inputField = document.getElementById("input");
+  getPlants();
 };
 
 const createPlantCard = (plants) => {
-    const plant = plants.find((plant) => {
-        if (inputField.value === plant.name) {
-            return plant
-        }
-    });
+  const plant = plants.find((plant) => {
+    if (inputField.value === plant.name) {
+      return plant;
+    }
+  });
 
+  const plantCard = document.createElement("div");
 
-    const plantCard = document.createElement('div');
+  plantCard.classList.add("plant-card");
 
-    plantCard.classList.add('plant-card');
-
-    plantCard.innerHTML = `
-        <img alt='plant' src=${plant.img} class="movie-cover"/>
-        <p class="plant-title">${plant.name}</p>
-        <div class="care-tips-container">
-            <ul>
-                <li>${plant.careTips[0].water}</li>
-                <li>${plant.careTips[1].sunlight}</li>
-                <li>${plant.careTips[2].difficulty}</li>
-            </ul>
+  plantCard.innerHTML = `
+    <div class="row">
+      <div class="column">
+        <div class="img-container">
+          <img 
+            alt='plant' 
+            src=${plant.img}
+            class="img-styles"
+          />
         </div>
-            <span id='upvote'>${plant.Like}</span>
-        <button onClick="updateLikes(${plant.id})">
-            Upvote
-        </button>
-    `
-    plantsContainer.appendChild(plantCard)
+        <div class="column-icons">
+          <h2 class="plant-title">
+            ${plant.name}
+          </h2>
+          <button 
+            onClick="updateLikes(${plant.id})"
+            class="upvote-btn"
+          >
+            <i 
+              class="fa fa-heart-o"
+              style="font-size:24px"
+            >
+            </i>
+            <span id='upvote' class="upvote-number-styles">
+              ${plant.Like}
+            </span>
+          </button>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <i 
+            style="font-size:24px" 
+            class="fa">&#xf043;
+          </i> 
+          ${plant.careTips[0].water}</li>
+        <li>
+          <i 
+            style="font-size:24px" 
+            class="fa">&#xf185;
+          </i>
+          ${plant.careTips[1].sunlight}
+        </li>
+        <li>
+          <i 
+            style="font-size:24px" 
+            class="fa">&#xf0e4;
+          </i>
+          ${plant.careTips[2].difficulty}
+        </li>
+      </ul>
+    </div>
+  `;
+  plantsContainer.appendChild(plantCard);
 };
 
 const updateLikeValue = (plant) => {
-    document.getElementById('upvote').innerHTML = `${plant.Like}`
-}
+  document.getElementById("upvote").innerHTML = `${plant.Like}`;
+};
 
-plantBtn.addEventListener('click', getPlantFact);
-form.addEventListener('submit', submitHandler);
+plantBtn.addEventListener("click", getPlantFact);
+form.addEventListener("submit", submitHandler);
