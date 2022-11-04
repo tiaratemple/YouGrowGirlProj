4894e6742a6ddf1a5855a6e7112343da41e072da
Tiara Temple <tmtemple7@gmail.com>
2022-11-04 12:43:19 -0500

    hopefully last axios change

--- public/main.js
+++ public/main.js
@@ -24,7 +24,7 @@ const getPlants = () => {
 };
 
 const updateLikes = (id) => {
-  axios.put(`${baseURL}/${id}`)
+  axios.put(`${baseURL}/plants/${id}`)
   .then(updateLikesCallback)
   .catch(errCallback);
 };
